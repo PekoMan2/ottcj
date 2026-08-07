b5c7711b711ec63cb20143264b2fce1743e4ecc7
Matej <hozlarmatej0@gmail.com>
2026-08-07 17:29:48 +0200

    chore: remove the nestjs backend and database stack

--- be/src/donio/donio.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DonioController } from './donio.controller';
-import { DonioService } from './donio.service';
-
-@Module({
-  controllers: [DonioController],
-  providers: [DonioService],
-})
-export class DonioModule {}
